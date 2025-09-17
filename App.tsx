195686d5fa94224aa26035f078202c3099f78860
jujuiwaki <julia.iwaki@aluno.lsb.com.br>
2025-09-16 21:03:30 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    site da juju 
+
+  <p>receitas de cookies e bolos</p>
+
+  <p>cookie tradicional 
+    <p>brookie 
+      <p>bolo de chocolate
+        <p>bolo de cenoura
+
+        </p>
+      </p>
+    </p>
+  </p>
+ </>
+  )
+}
+
+export default App
