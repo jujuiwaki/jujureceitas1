2b0201a01c6dfcb40da01c2f89b126603c54b014
jujuiwaki <julia.iwaki@aluno.lsb.com.br>
2025-09-16 21:06:13 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    site da juju 
+
+  <p>receitas de cookies e bolos</p>
+
+  <p>cookie tradicional 
+    <p>brookie 
+      <p>bolo de chocolate
+        <p>bolo de cenoura
+
+        </p>
+      </p>
+    </p>
+  </p>
+ </>
+  )
+}
+
+export default App
